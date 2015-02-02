bbcbbca1af816ca953235bf0019a2736e907ef4b
Nikita Trukhanov <nikitatrukhanov@MacBook-Pro-Nikita.local>
2015-02-02 13:16:57 +0500

    Refactor some code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ SampleApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  
+
   config.force_ssl = true
   config.serve_static_assets = true
 end
